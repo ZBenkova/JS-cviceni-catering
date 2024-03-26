ae6291cf35e2529d744b9500861d0ecb5e6797d8
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-03-26 22:36:24 +0100

    nove js

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
+const myFunction = (price, numberOfPeople) => {
+    const vysledek = price * numberOfPeople
+
+    return vysledek
+
+}
+
 const justFood = (numberOfPeople) => {
-    const price = 1090
-    let result = price * numberOfPeople
+    const mojePromenna = myFunction(1090, numberOfPeople)
 
     const message = `Catering od Just Food pro ` +
     numberOfPeople +
     ` lidi za ` +
-    result +
+    mojePromenna +
     ` kc.`
 
     return message
 } 
 
 const yourMama= (numberOfPeople) => {
-    const price = 3450
-    let result = price * numberOfPeople
+    const mojePromenna = myFunction(3450, numberOfPeople)
 
     const message = `Catering od Your Mama pro ` +
     numberOfPeople +
     ` lidi za ` +
-    result +
+    mojePromenna +
     ` kc.`
 
     return message
 } 
 
 const flavourMama= (numberOfPeople) => {
-    const price = 5500
-    let result = price * numberOfPeople
+    const mojePromenna = myFunction(6600, numberOfPeople)
 
     const message = `Catering od Flavour Mama pro ` +
     numberOfPeople +
     ` lidi za ` +
-    result +
+    mojePromenna +
     ` kc.`
     
     return message
@@ -42,6 +46,7 @@ const createEvent = (nameOfEvent, numberOfPeople, catering) => {
     const message = `Udalost ` +
         nameOfEvent +
         ` s ` +
+    
         cateringString
 
     return message
@@ -51,3 +56,4 @@ let paragraph = document.querySelector("#paragraph")
 paragraph.innerHTML += createEvent("inaugurace prezidenta", 23, flavourMama)
 
 console.log(createEvent("inaugurace prezidenta", 23, flavourMama))
+
